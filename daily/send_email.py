2320d6c849d118fa74cd9261d07da41c5abfd458
utsav2110 <solankiutsavdharmeshkumar@gmail.com>
2025-06-16 19:57:26 +0530

    for testing change in url

--- daily/send_email.py
+++ daily/send_email.py
@@ -2,7 +2,9 @@ import os
 import requests
 
 token = os.environ["EMAIL_TRIGGER_TOKEN"]
-trigger_url = f"https://your-app.streamlit.app/?trigger=email&token={token}"
+# trigger_url = f"https://your-app.streamlit.app/?trigger=email&token={token}"
+trigger_url = f"http://localhost:8501/?trigger=email&token={token}"
+
 
 r = requests.get(trigger_url)
 if r.status_code == 200:
