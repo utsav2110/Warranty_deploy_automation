e8f41eed3b75262e8eca36cbaab12fde7fbdd329
utsav2110 <solankiutsavdharmeshkumar@gmail.com>
2025-06-16 18:18:17 +0530

    Initial commit

---
+++ daily/delete_expired.py
@@ -0,0 +1,18 @@
+import psycopg2
+import os
+
+conn = psycopg2.connect(
+    host=os.environ["SUPABASE_HOST"],
+    dbname=os.environ["SUPABASE_DB"],
+    user=os.environ["SUPABASE_USER"],
+    password=os.environ["SUPABASE_PASS"],
+    port=5432
+)
+
+cur = conn.cursor()
+cur.execute("DELETE FROM warranty_items WHERE warranty_end_date < CURRENT_DATE;")
+conn.commit()
+cur.close()
+conn.close()
+
+print("✅ Expired items deleted from Supabase")
